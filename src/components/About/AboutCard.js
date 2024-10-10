ab5b83ddc576452e89ed46da4941377bba31d0b7
manukarthikv <122390232+manukarthikv@users.noreply.github.com>
2024-10-10 15:46:08 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,16 +8,21 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Manu Karthik</span>
+            from <span className="purple">Bengaluru, Karnataka.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am a recent Computer Science graduate from the Global Academy of Technology, Bengaluru, with hands-on experience in web development and machine learning.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I'm currently focused on building my technical skills and solving IT challenges.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            During my internship at Technofly Solutions as a Machine Learning Intern, I researched image processing algorithms and worked on a speech recognition project. I also gained web development experience as an intern at TechCiti Software Consulting, where I enhanced features for the MERN Estate Website and improved user engagement through data-driven solutions.
+            <br />
+            <br />
+            I am skilled in Python, SQL, Java, React, MongoDB, and Firebase, with a passion for creating real-world impactful solutions. Recently, I’ve been working on projects like knee osteoarthritis detection using deep learning and a university management system to automate administrative tasks.
+            <br />
+            <br />
+            Outside of work, I serve as the Vice President of Education at the Nagarabhavi Toastmasters Club, where I organize meetings and have won multiple public speaking contests.
           </p>
           <ul>
             <li className="about-activity">
@@ -34,7 +39,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Manu Karthik</footer>
         </blockquote>
       </Card.Body>
     </Card>
