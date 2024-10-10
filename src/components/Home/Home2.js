0b09705803c5f461c53b5653d1b5a990a2b91a8d
manukarthikv <122390232+manukarthikv@users.noreply.github.com>
2024-10-10 16:18:15 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,37 +19,36 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              Hi, I'm Manu Karthik! I'm a recent Computer Science graduate from 
+              the Global Academy of Technology, Bengaluru, with a CGPA of 8.96. 
+              I have hands-on experience in web development and machine learning.
               <br />
-              <br />I am fluent in classics like
+              <br />
+              I am skilled in{" "}
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple">HTML, CSS, JavaScript, Java, Python</b>
+              </i>{" "}
+              and proficient in frameworks like{" "}
+              <i>
+                <b className="purple">React, Node.js, Express.js, and Next.js.</b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
+              My experience includes internships at Technofly Solutions as a 
+              Data Science Intern and at TechCiti Software Consulting as a Web 
+              Developer Intern. I’ve worked on projects like{" "}
+              <b className="purple">Estate website</b>, 
+              <b className="purple">Knee Osteoarthritis Detection</b>, and 
+              <b className="purple">University Management System</b>.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              I am passionate about solving IT challenges and developing impactful 
+              real-world solutions, especially in the fields of{" "}
               <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+                <b className="purple">Web Technologies</b>
+              </i>{" "}
+              and{" "}
+              <b className="purple">Machine Learning</b>.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -62,42 +61,42 @@ function Home2() {
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className="purple">connect</span> with me
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/manukarthikv"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/your-twitter-handle"  // Add your Twitter handle
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiOutlineTwitter />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/manukarthikv/"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <FaLinkedinIn />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/your-instagram-handle" // Add your Instagram handle
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
@@ -112,4 +111,5 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
